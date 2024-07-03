659c37efc5043b2874027e783193fb08da70d974
chetanRegmi <chetanregmi5@gmail.com>
2024-07-03 22:52:07 +0800

    Add PDF text extraction functionality

---
+++ pdf_utils.py
@@ -0,0 +1,9 @@
+import PyPDF2
+
+def extract_text_from_pdf(file_path):
+    with open(file_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        text = ''
+        for page in reader.pages:
+            text += page.extract_text()
+    return text
